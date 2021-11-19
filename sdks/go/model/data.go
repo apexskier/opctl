3089859a33d803da4768f427a9e44bd6fdaf55ee
Cameron Little <cameron@remitly.com>
2021-11-19 11:50:32 -0700

    Remove commented code

--- sdks/go/model/data.go
+++ sdks/go/model/data.go
@@ -68,31 +68,6 @@ type Value struct {
 	String  *string                 `json:"string,omitempty"`
 }
 
-// // ValueNode interfaces with a value of any type
-// type ValueNode interface {
-// 	// ID returns a globally unique id for this value
-// 	ID() string
-// }
-
-// // FileValue interfaces with a file typed value
-// type FileValue interface {
-// 	GetReadSeekCloser() (ReadSeekCloser, error)
-
-// 	ValueNode
-// }
-
-// // DirValue interfaces with a directory typed value
-// type DirValue interface {
-// 	GetDescendant(
-// 		ref string,
-// 	) (
-// 		Value,
-// 		error,
-// 	)
-
-// 	ValueNode
-// }
-
 // Unbox unboxes a Value into a native go type
 func (value Value) Unbox() (interface{}, error) {
 	if value.Array != nil {
